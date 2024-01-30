e3a4637b7818e0e44dc8fb0ad54e4c05625d2806
razzz8210 <155446513+razzz8210@users.noreply.github.com>
2024-01-30 12:25:07 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const account_Id =36829
+let account_Email = "rajaiswa256@gmail.com"
+var account_Password=8989
+account_city="bhadohi"
+
+// account_Id=8278  // change not allowed
+
+account_Email="kjahj@gmail.com"
+account_Password=9891
+
+console.log(account_Id);
+console.table([account_Id,account_Email,account_Password,account_city])
