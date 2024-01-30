b5fe4197dbafdedb02180fea60ba9e14bd13c903
razzz8210 <155446513+razzz8210@users.noreply.github.com>
2024-01-30 13:42:05 +0000

    conversion of operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,3 +12,16 @@ console.log(valueInNumber)
 "33ahgjh"->NaN
 true->1
 */
+
+//***************************************************operation*********************** */
+let val=78
+let negVal=-val
+console.log(negVal)
+
+let str1="ram"
+let str2="ghd"
+let str3=str1+str2
+console.log(str3)
+
+console.log(1+3+"3")
+console.log("1"+3+3)
