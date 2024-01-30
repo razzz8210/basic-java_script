0d69e09f0747521997e840c6c20b388867a10594
razzz8210 <155446513+razzz8210@users.noreply.github.com>
2024-01-30 13:09:28 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";  // treat all js code as newer version
+/*
+alert("hello")    // we are using node js not browser
+alert(3+5)
+
+*/
+console.log(3+3)
+console.log("Raj Jaiswal")
+
+let name="hsjgfjh"
+let roll_no=65567     //number (bigint) for large number
+let isloggedin=false
+let state = null
+
+console.log(typeof "ram")
