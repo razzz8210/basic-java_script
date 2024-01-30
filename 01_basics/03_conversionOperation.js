1bafdaf2cab46abd391eb9dd49f69851acdb1845
razzz8210 <155446513+razzz8210@users.noreply.github.com>
2024-01-30 13:27:45 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score=null
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*     conversion
+"33"->33
+"33ahgjh"->NaN
+true->1
+*/
